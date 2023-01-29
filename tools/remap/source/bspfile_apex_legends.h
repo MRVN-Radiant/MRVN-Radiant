32792a617b6bb7b70c18ecbe9b341b22844ad044
F1F7Y <filip.bartos07@proton.me>
2023-01-29 22:35:36 +0100

    Remove unused apex stubs

--- tools/remap/source/bspfile_apex_legends.h
+++ tools/remap/source/bspfile_apex_legends.h
@@ -149,36 +149,22 @@ namespace ApexLegends {
         inline std::vector<LevelInfo_t>         levelInfo;
 
         // Stubs
-        inline std::vector<uint8_t>  textureData_stub;
-        inline std::vector<uint8_t>  vertices_stub;
         inline std::vector<uint8_t>  lightprobeParentInfos_stub;
         inline std::vector<uint8_t>  shadowEnvironments_stub;
         inline std::vector<uint8_t>  models_stub;
-        inline std::vector<uint8_t>  surfaceNames_stub;
         inline std::vector<uint8_t>  contentsMasks_stub;
         inline std::vector<uint8_t>  surfaceProperties_stub;
         inline std::vector<uint8_t>  bvhNodes_stub;
         inline std::vector<uint8_t>  bvhNodeLeafData_stub;
-        inline std::vector<uint8_t>  entityPartitions_stub;
-        inline std::vector<uint8_t>  vertexNormals_stub;
-        inline std::vector<uint8_t>  gameLump_stub;
         inline std::vector<uint8_t>  unknown25_stub;
         inline std::vector<uint8_t>  unknown26_stub;
         inline std::vector<uint8_t>  unknown27_stub;
         inline std::vector<uint8_t>  cubemaps_stub;
         inline std::vector<uint8_t>  worldLights_stub;
-        inline std::vector<uint8_t>  vertexUnlit_stub;
-        inline std::vector<uint8_t>  meshIndicies_stub;
-        inline std::vector<uint8_t>  meshes_stub;
-        inline std::vector<uint8_t>  meshBounds_stub;
-        inline std::vector<uint8_t>  materialSort_stub;
         inline std::vector<uint8_t>  lightmapHeaders_stub;
         inline std::vector<uint8_t>  tweakLights_stub;
         inline std::vector<uint8_t>  lightmapDataSky_stub;
         inline std::vector<uint8_t>  csmAABBNodes_stub;
-        inline std::vector<uint8_t>  cellAABBNodes_stub;
-        inline std::vector<uint8_t>  objReferences_stub;
-        inline std::vector<uint8_t>  objReferenceBounds_stub;
         inline std::vector<uint8_t>  lightmapDataRTLPage_stub;
     }
 }
