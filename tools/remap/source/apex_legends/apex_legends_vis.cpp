f8f47e498f5ddde5f9201a4aadf4d6ce42c1b269
F1F7Y <64418963+F1F7Y@users.noreply.github.com>
2023-05-11 13:24:58 +0200

    r5 gamelump

--- tools/remap/source/apex_legends/apex_legends_vis.cpp
+++ tools/remap/source/apex_legends/apex_legends_vis.cpp
@@ -23,6 +23,7 @@
 
 
 #include "../remap.h"
+#include "../model.h"
 #include "../bspfile_abstract.h"
 #include <ctime>
 
@@ -333,4 +334,91 @@ void ApexLegends::EmitLevelInfo() {
         if( model.meshCount )
             li.modelCount++;
     }
+}
+
+/*
+    EmitStaticProp()
+    Emits a static prop
+*/
+void ApexLegends::EmitStaticProp(entity_t &e) {
+    const char *model = e.valueForKey("model");
+    int16_t pathIdx = -1;
+    MinMax minmax;
+
+    std::vector<const AssMeshWalker*> meshes = LoadModelWalker( model, 0 );
+    if(meshes.empty()) {
+        Sys_Warning("Failed to load model: %s\n", model);
+        return;
+    }
+
+    Sys_Printf("Emitting static prop: %s\n", model);
+
+    for(std::size_t i = 0; i < Titanfall::Bsp::gameLumpPaths.size(); i++) {
+        Titanfall::GameLumpPath_t &path = Titanfall::Bsp::gameLumpPaths.at(i);
+
+        if(!string_compare_nocase(path.path, model)) {
+            pathIdx = i;
+            break;
+        }
+    }
+
+    if(pathIdx == -1) {
+        Titanfall2::Bsp::gameLumpPathHeader.numPaths++;
+        Titanfall::GameLumpPath_t &path = Titanfall::Bsp::gameLumpPaths.emplace_back();
+        strncpy(path.path, model, 127);
+        pathIdx = Titanfall::Bsp::gameLumpPaths.size() - 1;
+    }
+
+    Titanfall2::Bsp::gameLumpPropHeader.numProps++;
+    Titanfall2::Bsp::gameLumpPropHeader.unk0++;
+    Titanfall2::Bsp::gameLumpPropHeader.unk1++;
+    Titanfall2::GameLumpProp_t &prop = Titanfall2::Bsp::gameLumpProps.emplace_back();
+    Vector3 origin;
+    Vector3 angles;
+    if(!e.read_keyvalue(origin, "origin")) {
+        origin.x() = 0.0f;
+        origin.y() = 0.0f;
+        origin.z() = 0.0f;
+    }
+    if(!e.read_keyvalue(angles, "angles")) {
+        angles.x() = 0.0f;
+        angles.y() = 0.0f;
+        angles.z() = 0.0f;
+    }
+
+    prop.origin = origin;
+    prop.angles = angles;
+    prop.scale = 1.0f;
+    prop.modelName = pathIdx;
+    prop.solid = 6;
+    prop.flags = 84;
+    prop.envCubemap = -1;
+    prop.fade_scale = -1.0f;
+    prop.cpu_level[0] = -1.0f;
+    prop.cpu_level[1] = -1.0f;
+    prop.gpu_level[0] = -1.0f;
+    prop.gpu_level[1] = -1.0f;
+    prop.unk.x() = 1.0f;
+    prop.unk.y() = 0.0f;
+    prop.unk.z() = 0.0f;
+
+    for ( const auto mesh : meshes )
+    {
+        mesh->forEachFace( [&minmax, &origin]( const Vector3 ( &xyz )[3], const Vector2 ( &st )[3] ){
+			minmax.extend(xyz[0] + origin);
+            minmax.extend(xyz[1] + origin);
+            minmax.extend(xyz[2] + origin);
+		} );
+    }
+
+    Shared::visRef_t &ref = Shared::visRefs.emplace_back();
+
+    ref.minmax = minmax;
+    ref.index = ApexLegends::Bsp::models.at(0).meshCount + Titanfall2::Bsp::gameLumpProps.size() - 1;
+}
+
+void ApexLegends::SetupGameLump() {
+    Titanfall2::Bsp::gameLumpHeader.version = 1;
+    memcpy( &Titanfall2::Bsp::gameLumpHeader.ident, "prps", 4 );
+    Titanfall2::Bsp::gameLumpHeader.gameConst = 3080192;
 }
